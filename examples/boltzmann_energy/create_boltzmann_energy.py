5f37ff66337955cb940bf339f320d459dc63fba8
Alex Goeßmann <alex.goessmann@web.de>
2025-01-15 18:17:44 +0100

    Tested Boltzmann energy based on random 2x2 weights

--- examples/boltzmann_energy/create_boltzmann_energy.py
+++ examples/boltzmann_energy/create_boltzmann_energy.py
@@ -10,11 +10,12 @@ def get_boltzmann_neuronNameDict(atomList, neurKey):
     """
     Provides a names dictionary of a logical neuron parametrizing boltzmann energies
     """
-    return {neurKey: [["and", "lpas"], atomList, atomList]}
+    return {neurKey: [["eq", "lpas"], atomList, atomList]}
 
 
-def get_boltzmann_selectionNetwork(atomList, neurKey="boltzmannNeur"):
-    return encoding.create_architecture(get_boltzmann_neuronNameDict(atomList, neurKey))
+def get_boltzmann_selectionNetwork(atomList, neurKey="boltzmannNeur", coreType="NumpyCore"):
+    return encoding.create_architecture(get_boltzmann_neuronNameDict(atomList, neurKey), headNeuronNames=[neurKey],
+                                        coreType=coreType)
 
 
 def get_boltzmann_parameterCore(weightMatrix, biasVector, neurKey="boltzmannNeur", coreType="NumpyCore"):
@@ -24,7 +25,7 @@ def get_boltzmann_parameterCore(weightMatrix, biasVector, neurKey="boltzmannNeur
     weightIterator = [(weightMatrix[i, j], {neurKey + "_asVar": 0, neurKey + "_p0_vsVar": i, neurKey + "_p1_vsVar": j})
                       for i, j in np.ndindex(weightMatrix.shape)]
     biasIterator = [
-        (biasVector[i], {neurKey + "_asVar": 0, neurKey + "_p0_vsVar": i, neurKey + "_p1_vsVar": 0})
+        (biasVector[i], {neurKey + "_asVar": 1, neurKey + "_p0_vsVar": i, neurKey + "_p1_vsVar": 0})
         for i in np.ndindex(weightMatrix.shape[0])]
     return engine.create_from_slice_iterator(shape=[2] + list(weightMatrix.shape),
                                              colors=[neurKey + "_asVar", neurKey + "_p0_vsVar", neurKey + "_p1_vsVar"],
@@ -32,19 +33,45 @@ def get_boltzmann_parameterCore(weightMatrix, biasVector, neurKey="boltzmannNeur
                                              coreType=coreType)
 
 
+def reduce_matrix_to_upperOffDiagonalTriangle(matrix):
+    ## Those are tautologies
+    for i in range(atomorder):
+        matrix[i, i] = 0
+
+    ## Those are redundant by symmetry of bicond
+    for i in range(atomorder):
+        for j in range(i):
+            matrix[i, j] = 0
+    return matrix
+
+
 if __name__ == "__main__":
     """
     Test instance of weights and potentials
     """
     atomorder = 2
-    values = np.random.rand(atomorder, atomorder)
-    weightMatrix = engine.get_core("NumpyCore")(values=values, colors=["blub", "blub2"])
-    biasVector = engine.get_core("NumpyCore")(values=np.random.rand(atomorder), colors=["blub3"])
+    weightValues = reduce_matrix_to_upperOffDiagonalTriangle(np.random.rand(atomorder, atomorder))
+
+    biasValues = np.random.rand(atomorder)
+    weightMatrix = engine.get_core("NumpyCore")(values=weightValues, colors=["blub", "blub2"])
+    biasVector = engine.get_core("NumpyCore")(values=biasValues, colors=["blub3"])
 
     """
     Create a boltzmann energy decomposition
     """
-    engine.draw_factor_graph(
-        {**get_boltzmann_selectionNetwork(atomList=["a" + str(i) for i in range(atomorder)]),
-         "parCore": get_boltzmann_parameterCore(weightMatrix, biasVector, coreType="PandasCore")}
-    )
+    coreType = "NumpyCore"
+    contractionMethod = "NumpyEinsum"
+    energyCoreDict = {
+        **get_boltzmann_selectionNetwork(atomList=["a" + str(i) for i in range(atomorder)], coreType=coreType),
+        "parCore": get_boltzmann_parameterCore(weightMatrix, biasVector, coreType=coreType)}
+
+    contracted = engine.contract(coreDict=energyCoreDict, openColors=["a" + str(i) for i in range(atomorder)],
+                                 contractionMethod=contractionMethod)
+
+    ## When both atoms are false, only their bicond is true
+    assert contracted[0, 0] == weightValues[0, 1]
+    ## When only one is true, only one atomic formula is true
+    assert contracted[0, 1] == biasValues[1]
+    assert contracted[1, 0] == biasValues[0]
+    ## When both atoms are true, their bicond and their atomic formulas are true
+    assert contracted[1, 1] == weightValues[0, 1] + biasValues[0] + biasValues[1]
