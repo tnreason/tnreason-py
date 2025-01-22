a01a06da601ace1aa2bee3d735c9a21ad2723a94
Alex Goeßmann <alex.goessmann@web.de>
2025-01-22 21:12:42 +0100

    Decomposed parametercores and observed convergence of the QUBO fitting experiment

--- experiments/collapsed_approximation/ls_optimize.py
+++ experiments/collapsed_approximation/ls_optimize.py
@@ -9,18 +9,23 @@ sparsityOrder = 2
 varColList = ["a" + str(k) + aSuf for k in range(varNum)]
 
 selCores = slsel.create_slice_selecting_tn(variableColorList=varColList, sparsityOrder=2)
-parCore = engine.create_trivial_core(name="parCore",
-                                     shape=[3 for _ in range(sparsityOrder)] + [2 for _ in range(sparsityOrder)],
-                                     colors=slsel.get_selection_colors(sparsityOrder=2))
+
+parCores = {"actParCore": engine.create_basis_core(name="actParCore",
+                                                   shape=[2 for _ in range(sparsityOrder)],
+                                                   colors=['posNeur0_p0_vsVar', 'posNeur1_p0_vsVar'],
+                                                   numberTuple=(0, 1)),
+            "pCoreVar": engine.create_trivial_core(name="parCoreVar",
+                                                   shape=[3 for _ in range(sparsityOrder)],
+                                                   colors=['posNeur0_asVar', 'posNeur1_asVar'])
+            }
 
 tarCores = encoding.create_formulas_cores({"w1": ["imp", "a0", "a1"]})
 
 optimizer = als.ALS(networkCores={**selCores,
-                                  "pCore": parCore},
+                                  **parCores},
                     targetCores=tarCores,
                     importanceColors=varColList)
-print(optimizer.alternating_optimization(["pCore"], sweepNum=1, computeResiduum=True))
+residuum = optimizer.alternating_optimization(["pCoreVar", "actParCore"], sweepNum=10, computeResiduum=True)
 
-## Should be the same, since selection architecture fits all in this case, but is not
-print(engine.contract({**selCores, "pCore": optimizer.networkCores["pCore"]}, openColors=varColList).values)
+print(engine.contract(optimizer.networkCores, openColors=varColList).values)
 print(engine.contract({**tarCores}, openColors=varColList).values)
