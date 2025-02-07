dc57da823b8ff6a085f8cb4f3dbc05832bdc9abd
Alex Goeßmann <alex.goessmann@web.de>
2025-02-07 17:55:50 +0100

    Added parameter calibration

--- experiments/collapsed_approximation/greedy_thresholding.py
+++ experiments/collapsed_approximation/greedy_thresholding.py
@@ -2,7 +2,9 @@ from experiments.collapsed_approximation import slice_selection_architecture as 
 
 from tnreason import engine, algorithms
 
-def greedy_gradient_step(energyDicts, currentParameters, varColList, sparsityOrder=2, optimizationMethod="numpyArgMax", contrastCores=[]):
+
+def greedy_gradient_step(energyDicts, currentParameters, varColList, sparsityOrder, optimizationMethod="numpyArgMax",
+                         contrastCores=[]):
     """
     Update step of a
         energyDicts: Representation of tensor to be approximated
@@ -12,11 +14,48 @@ def greedy_gradient_step(energyDicts, currentParameters, varColList, sparsityOrd
         currentParameters,
         slsel.create_slice_selecting_tn(variableColorList=varColList, sparsityOrder=sparsityOrder)
     )
-    gradient = engine.sum_contract(weightedCoreDicts=energyDicts + [(-1, {"cAp": currentApproximation})] + contrastCores,
-                                   backCores=slsel.create_slice_selecting_tn(variableColorList=varColList,
-                                                                             sparsityOrder=sparsityOrder),
-                                   openColors=slsel.get_selection_colors(sparsityOrder))
+    gradient = engine.sum_contract(
+        weightedCoreDicts=energyDicts + [(-1, {"cAp": currentApproximation})] + contrastCores,
+        backCores=slsel.create_slice_selecting_tn(variableColorList=varColList,
+                                                  sparsityOrder=sparsityOrder),
+        openColors=slsel.get_selection_colors(sparsityOrder))
     return gradient, algorithms.core_based_optimize(gradient, optimizationMethod=optimizationMethod)
+
+
+def update_parameters(energyDicts, currentParameters, varColList, sparsityOrder, selPosList, sweepNum):
+    for _ in range(sweepNum):
+        for selPosDict in selPosList:
+            currentParameters[selPosDict] = calculate_coordinate_update(energyDicts, {"pCore": currentParameters}, varColList,
+                                                                        sparsityOrder, selPosDict)
+            print(calculate_distance(energyDicts, {"pCore": currentParameters}, varColList, sparsityOrder))
+
+def calculate_distance(energyDicts, currentParameters, varColList, sparsityOrder):
+    currentApproximation = engine.contract(
+        coreDict={**currentParameters,
+                  **slsel.create_slice_selecting_tn(variableColorList=varColList, sparsityOrder=sparsityOrder)},
+        openColors=varColList
+    )
+    residuum = engine.sum_contract(weightedCoreDicts=energyDicts + [(-1, {"cAp": currentApproximation})],
+                                   openColors=varColList)
+    return np.linalg.norm(residuum.values)
+
+def calculate_coordinate_update(energyDicts, currentParameters, varColList, sparsityOrder, selPosDict):
+    currentApproximation = engine.contract(
+        coreDict={**currentParameters,
+                  **slsel.create_slice_selecting_tn(variableColorList=varColList, sparsityOrder=sparsityOrder)},
+        openColors=varColList
+    )
+    residuum = engine.sum_contract(weightedCoreDicts=energyDicts + [(-1, {"cAp": currentApproximation})],
+                                   openColors=varColList)
+    overlap = engine.contract(coreDict={"res": residuum, **slsel.create_slice_selecting_tn(variableColorList=varColList,
+                                                                                           sparsityOrder=sparsityOrder)},
+                              colorEvidenceDict=selPosDict,
+                              openColors=["trivial"], dimensionDict={"trivial": 1})[0]
+    worldCount = engine.contract(coreDict=slsel.create_slice_selecting_tn(variableColorList=varColList,
+                                                                          sparsityOrder=sparsityOrder),
+                                 colorEvidenceDict=selPosDict,
+                                 openColors=["trivial"], dimensionDict={"trivial": 1})[0]
+    return overlap / worldCount
 
 
 if __name__ == "__main__":
@@ -31,27 +70,36 @@ if __name__ == "__main__":
     selColors = slsel.get_selection_colors(sparsityOrder)
     selShape = [value for _, value in slsel.get_selection_dimDict(sparsityOrder, varNum).items()]
 
-    energyDict = [(1, encoding.create_formulas_cores(expressionsDict={"w1" : ["and","a0","a1"]}))]
-    currentParameters = engine.get_core("NumpyCore")(#values=np.zeros(shape=[2 for _ in range(varNum)]),
-                                                     colors=slsel.get_selection_colors(sparsityOrder),
-                                                     shape=selShape)
+    energyDict = [(1, encoding.create_formulas_cores(expressionsDict={"w1": ["and", "a0", "a1"]}))]
+    currentParameters = engine.get_core("NumpyCore")(  # values=np.zeros(shape=[2 for _ in range(varNum)]),
+        colors=slsel.get_selection_colors(sparsityOrder),
+        shape=selShape)  # empty initialization
 
     gradient, result = greedy_gradient_step(
-        energyDicts=energyDict,#[(1, {"tCore": engine.get_core("NumpyCore")(colors=varColList, shape=[2 for _ in range(varNum)])})],
+        energyDicts=energyDict,
+        # [(1, {"tCore": engine.get_core("NumpyCore")(colors=varColList, shape=[2 for _ in range(varNum)])})],
         currentParameters={"pCore": currentParameters},
         varColList=varColList,
         sparsityOrder=sparsityOrder,
-        contrastCores=[(-1/2,{})])
+        contrastCores=[(-1 / 2, {})])
 
-    print(result)
-    selCores = slsel.create_slice_selecting_tn(variableColorList=varColList, sparsityOrder=2)
-    print(gradient[0,0,1,0])
-    print(gradient[1,0,1,1])
-    print(gradient[2,1,2,1])
+    update_parameters(energyDict, currentParameters, varColList, sparsityOrder, [result], 10)
 
-    print(currentParameters.shape)
-    print(currentParameters.values)
-    currentParameters[result] = 1
-    print("aft")
-    print(currentParameters.values)
-    #engine.draw_factor_graph(selCores)
+    print(currentParameters[result])
+    # currentParameters[result] = calculate_coordinate_update(energyDict, {"pCore": currentParameters}, varColList,
+    #                                                         sparsityOrder, result)
+    # print(currentParameters.values)
+    #
+    # print(currentParameters[result])
+    # exit()
+    # selCores = slsel.create_slice_selecting_tn(variableColorList=varColList, sparsityOrder=2)
+    # print(gradient[0, 0, 1, 0])
+    # print(gradient[1, 0, 1, 1])
+    # print(gradient[2, 1, 2, 1])
+    #
+    # print(currentParameters.shape)
+    # print(currentParameters.values)
+    # currentParameters[result] = 1
+    # print("aft")
+    # print(currentParameters.values)
+    # # engine.draw_factor_graph(selCores)
