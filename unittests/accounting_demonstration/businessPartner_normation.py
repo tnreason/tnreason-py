0cdcd050d331e5c925b66e6ad9bd53fa574f3c68
Alex Goeßmann <alex.goessmann@web.de>
2025-07-21 15:00:46 +0200

    Adjusted business partner unittest to run along the other unittests

--- unittests/accounting_demonstration/businessPartner_normation.py
+++ unittests/accounting_demonstration/businessPartner_normation.py
@@ -1,5 +1,7 @@
+import unittest
 import pandas as pd
-
+from tnreason.engine import workload_to_pandas as wtp
+from tnreason import engine
 
 bp_dict = {'BUSINESSPARTNER': {0: 28,
   1: 28,
@@ -409,3 +411,40 @@ def get_result_df(normedCoreValues, basisCoreValues, termCore):
         dfs_dict.append(result_dict)
 
     return pd.DataFrame(dfs_dict)
+
+class ToyAccountingBPTest(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.DataFrame(bp_dict)
+
+        # Define labels and features
+        self.labels = ['ACCOUNT_tV', 'BP_ACCOUNT_tV']
+        self.features = ['BUSINESSPARTNER_tV', 'VAT_tV']
+
+        # Create TermCore object
+        self.termCore = wtp.PandasTermCore(valueDf=self.df)
+        self.pdCore = engine.convert(self.termCore, "PolynomialCore")
+
+    def test_script_execution(self):
+        """Test if the script runs successfully."""
+        # Core contraction
+        smallCore = engine.contract(
+            coreDict={"pdCore": self.pdCore.clone()},
+            openColors=self.features + self.labels,
+            contractionMethod="CorewiseContractor"
+        )
+        smallCore.add_identical_slices()
+
+        normedCore = engine.normalize(
+            {"core": smallCore.clone()},
+            outColors=self.labels,
+            inColors=self.features,
+            contractionMethod="CorewiseContractor",
+            coreType="PolynomialCore"
+        )
+
+        resultDf = get_result_df(normedCore.values, smallCore.values, self.termCore)
+
+        self.assertTrue(not resultDf.empty, "The result DataFrame is empty.")
+        self.assertTrue(resultDf.shape[0] == 5, "The DataFrame does not have 5 rows.")
+        self.assertTrue(resultDf[(resultDf["BUSINESSPARTNER"]=="24") & (resultDf["VAT"]=="0")]["probability %"].iloc[0] == 94, "The probability for tested row is not 94")
+        self.assertTrue("3" not in resultDf["VAT"].values, "Value 3 is unexpectedly found in column 'VAT'.")
