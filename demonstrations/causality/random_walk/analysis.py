d0be61282753c94038a813efc83227cdec30572f
Alex Goessmann <alex.goessmann@web.de>
2026-06-26 21:57:52 +0200

    Added conditional queries to causal random walk

--- demonstrations/causality/random_walk/analysis.py
+++ demonstrations/causality/random_walk/analysis.py
@@ -5,5 +5,7 @@ from tnreason import  engine
 def get_marginal_matrix(cores, margVariables, dim):
     margs = np.empty((len(margVariables), dim))
     for i, margVar in enumerate(margVariables):
-        margs[i] = engine.contract(cores, openColors=[margVar]).values
+        rawMarginals = engine.contract(cores, openColors=[margVar]).values
+
+        margs[i] = 1/np.sum(rawMarginals) * rawMarginals
     return margs
--- demonstrations/causality/random_walk/test_intervention.py
+++ demonstrations/causality/random_walk/test_intervention.py
@@ -5,19 +5,34 @@ from tnreason import engine
 T = 5
 dim = 4
 
-## Without intervention
+## Rung 1 : Association - Marginals, conditional probabilities
 
-print(an.get_marginal_matrix(cr.get_randomWalkCores(T=T, dim=dim, withIntervention=True), [f"X_{i}" for i in range(T)],
+cores = cr.get_randomWalkCores(T=T, dim=dim, withIntervention=False)
+
+print(an.get_marginal_matrix(cr.get_randomWalkCores(T=T, dim=dim, withIntervention=False), [f"X_{i}" for i in range(T)],
                              dim=dim))
 
+
+
+conditionedVar = 2
+conditionedState = 1
+print(an.get_marginal_matrix({**cr.get_randomWalkCores(T=T, dim=dim, withIntervention=False),
+    "condCore": engine.create_from_slice_iterator(
+        colors=[f"X_{conditionedVar}"],
+        shape=[dim],
+        sliceIterator=[(1, {f"X_{conditionedVar}": conditionedState})])
+}, [f"X_{i}" for i in range(T)],
+    dim=dim))
+
+## Rung 2 : Intervention
 ## With intervention
-interventedVar = 1
-interventedState = 2
+interventedVar = 2
+interventedState = 1
 
 print(an.get_marginal_matrix({**cr.get_randomWalkCores(T=T, dim=dim, withIntervention=True),
     "intCore": engine.create_from_slice_iterator(
         colors=[f"D_{interventedVar}"],
-        shape=[dim+1],
+        shape=[dim + 1],
         sliceIterator=[(1, {f"D_{interventedVar}": interventedState})]
     )
 }, [f"X_{i}" for i in range(T)], dim=dim))
